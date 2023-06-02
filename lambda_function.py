050d9e0a1c407055bad06d51ea6b2191381a7032
Junior Marques <junior.uniseb8@gmail.com>
2023-06-02 17:34:31 -0300

    update

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,6 @@ def lambda_handler(event, context):
     log.funcLog("Retornando um log: %s" % event)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from BeOnUP!')
+        'body': json.dumps('Hello from BeOnUP!'),
+        'Headers': json.dumps({"Content-Type": "application/json"}),
     }
--- log.py
+++ log.py
@@ -1,2 +1,6 @@
+import os
+
 def funcLog (messagem): 
+    ju_var = os.environ.get('JR_VAR')
+    print (ju_var)
     print ("Adicionando um log.", messagem)
